2fe895141f30f152ca15a4a893ebe74361e751e8
Prajyot Satarde <prajyotsatarde2@gmail.com>
2026-09-09 22:46:31 +0530

    Another patch for migration issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,11 +3,14 @@ from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
 from . import models
+from .database import Base, engine
 from .routers.pass_router import router as pass_router
 from .routers.voting_router import router as voting_router
 from .routers.admin_router import router as admin_router
 from .routers.events_router import router as events_router
 
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Freshers Backend")
 app.mount("/uploads", StaticFiles(directory=Path(__file__).resolve().parent.parent / "data" / "uploads", check_dir=False), name="uploads")
